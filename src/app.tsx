65d52769de284ec008adf812d4c7b4048f680a55
moonhr <moonhr153@gmail.com>
2024-07-09 11:46:30 +0900

    test

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+
+const Minam = () => {
+  return (
+    <h1>공욱재</h1>
+  )
+}
+
 const App: React.FC = () => {
   return (
     <>
       <div>Hello World!</div>
+      <Minam />
     </>
   );
 };
