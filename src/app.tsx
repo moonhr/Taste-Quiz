4be5c6a9bb5b8c9117bbd5281d187fbfde5553d0
moonhr <moonhr153@gmail.com>
2024-07-16 10:46:51 +0900

    가상dom초기화

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import minam from "../static/basic-data"
-import hoyeong from '../static/hoyeong'
-interface textnode {
-  textnode : string
-}
-import UlLi from './ulli'
-
-const Minam : React.FC<textnode> = ({textnode}) => {
-  return (
-    <h1>{textnode}</h1>
-  )
-}
+import buttonPractice from "./component/button";
 
 const App: React.FC = () => {
   return (
     <>
       <div>Hello World!</div>
-      <Minam textnode={minam} />
-      <UlLi listItem={hoyeong} />
+      <buttonPractice className='' {'text'} />
     </>
   );
 };
