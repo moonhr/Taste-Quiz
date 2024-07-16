f2fc7186e07e852d8ac3503647ceed97591f2fb7
moonhr <moonhr153@gmail.com>
2024-07-16 10:55:10 +0900

    버튼 컴포넌트 확인

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import buttonPractice from "./component/button";
+import Button from "./component/button";
 
 const App: React.FC = () => {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
-    <>
-      <div>Hello World!</div>
-      <buttonPractice className='' {'text'} />
-    </>
+    <div className="p-4">
+      <h1 className="mb-4 text-2xl">Welcome to My App</h1>
+      <Button
+        onClick={handleClick}
+        label="Click Me"
+        className="text-white bg-blue-500 hover:bg-blue-700"
+      />
+    </div>
   );
 };
-
 export default App;
